f19e5abec9d695906eaa6ee6ada1ce57bd368dcc
Valera Trusevich <truetnoth@gmail.com>
2022-04-14 15:48:46 +0300

    badge

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const welcome = () => {
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
+const welcome = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default welcome;
