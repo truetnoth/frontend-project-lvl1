c2f3cfa71dc2869e050e4644c15bf691f2bf2a74
Valera Trusevich <truetnoth@gmail.com>
2022-04-17 11:58:40 +0300

    fixing lint errors

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
-	return name;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default welcome;
--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,32 +1,31 @@
 import {
-	generateRandom,
-	ROUND_NUMBER,
-	showFailMessage,
-	checkAnswer,
-	userAnswer,
-	isPrime,
-  } from '../index.js';
-  
-  const isPrimeGame = (userName) => {
-	console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-	let questionCount = 0;
-  
-	for (questionCount; questionCount < ROUND_NUMBER; questionCount += 1) {
-	  const askNumber = generateRandom();
-	  const correctAnswer = isPrime(askNumber) ? 'yes' : 'no';
-  
-	  console.log(`Question: ${askNumber}`);
-  
-	  const answer = userAnswer();
-  
-	  if (answer !== correctAnswer) {
-		showFailMessage(answer, correctAnswer, userName);
-		break;
-	  } else console.log('Correct!');
-	}
-  
-	checkAnswer(questionCount, userName);
-  };
-  
-  export default isPrimeGame;
-  
+  generateRandom,
+  ROUND_NUMBER,
+  showFailMessage,
+  checkAnswer,
+  userAnswer,
+  isPrime,
+} from '../index.js';
+
+const isPrimeGame = (userName) => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let questionCount = 0;
+
+  for (questionCount; questionCount < ROUND_NUMBER; questionCount += 1) {
+    const askNumber = generateRandom();
+    const correctAnswer = isPrime(askNumber) ? 'yes' : 'no';
+
+    console.log(`Question: ${askNumber}`);
+
+    const answer = userAnswer();
+
+    if (answer !== correctAnswer) {
+      showFailMessage(answer, correctAnswer, userName);
+      break;
+    } else console.log('Correct!');
+  }
+
+  checkAnswer(questionCount, userName);
+};
+
+export default isPrimeGame;
