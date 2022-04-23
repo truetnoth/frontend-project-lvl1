986042f9cf29dcbb59904a3e653348cbcfdc1f38
Valera Trusevich <truetnoth@gmail.com>
2022-04-23 17:13:06 +0300

    small fix of functions order in index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,6 @@ export const ROUND_NUMBER = 3;
 export const userAnswer = () => readlineSync.question('Your answer: ');
 export const generateRandom = (limit = 100) => Math.floor(Math.random() * limit);
 
-export const countGcd = (firstNumber, secondNumber) => {
-  if (!secondNumber) {
-    return firstNumber;
-  }
-
-  return countGcd(secondNumber, firstNumber % secondNumber);
-};
-
 export const checkAnswer = (questionCount, userName) => {
   if (questionCount === ROUND_NUMBER) {
     console.log(`Congratulations, ${userName}!`);
@@ -24,6 +16,14 @@ export const checkAnswer = (questionCount, userName) => {
 
 export const showFailMessage = (answer, correctAnswer, userName) => {
   console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`);
+};
+
+export const countGcd = (firstNumber, secondNumber) => {
+  if (!secondNumber) {
+    return firstNumber;
+  }
+
+  return countGcd(secondNumber, firstNumber % secondNumber);
 };
 
 export const generateRandomSequence = () => {
